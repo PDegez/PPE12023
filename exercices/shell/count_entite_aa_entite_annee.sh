55063efacf65e72a72d57ea15100ec561967b792
PDegez <pauline.degez@gmail.com>
2023-10-18 16:53:42 +0200

    ajout des boucles de vérifications d'arguments aux scripts, séance 5

---
+++ exercices/shell/count_entite_aa_entite_annee.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+#script qui donne le nombre d'entité par année pour les fichiers triés (cours shell)
+ENTITE=$1
+ANNEE=$2
+
+#boucle de vérification du nombre d'arguments
+if [ $# -ne 2 ]
+then
+	echo "Ce script a besoin de deux arguments : <ENTITE> <ANNEE>. Reessayez."
+	exit
+fi
+
+#Script
+echo "Nombre de $ENTITE pour l année $ANNEE :"
+cat ~/Documents/Cours/PPE/fichiers_tries/ann/$ANNEE/*/*.ann|grep "$ENTITE"|cut -f 3|sort|uniq -c|wc -l
+
+
---
+++ exercices/shell/count_entite_trois_ans_a_entite.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+#script qui donne le nombre d'entité par année pour les 3 années du fichiers triés (cours shell)
+
+#Vérification du nombre d'arguments
+if [ $# -ne 1 ]
+then
+    echo "Ce script a besoin d'un argument pour fonctionner : <ENTITE>. Reessayez"
+    exit
+fi
+
+#Script boucle du script count_entite_aa_entite_annee.sh
+
+ENTITE=$1
+ANNEE=2016
+while [ -d ~/Documents/Cours/PPE/fichiers_tries/ann/$ANNEE ]
+do
+    bash count_entite_aa_entite_annee.sh $1 $ANNEE
+    ANNEE=$(expr $ANNEE + 1)
+done
+
