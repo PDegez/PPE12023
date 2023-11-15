092569ae2679344ceb6bc3bc3b7f4f8cb1aae429
PDegez <pauline.degez@gmail.com>
2023-11-15 09:41:10 +0100

    version corrigée

--- miniprojet/programmes/traitement_texte.sh
+++ miniprojet/programmes/traitement_texte.sh
@@ -14,4 +14,7 @@ then
 fi
 
 #traitement du texte : suppression ponctuation | suppression guillemets fr | supperssion tiret fr | transformation suite d'espace en un seul espace | transformation espace en retour ligne | transformation maj en min
-cat $texte_brut | tr -d [:punct:] | tr -d "«" | tr -d "—" | tr -s " " | tr " " "\n" | tr -s "\n" | tr [:upper:] [:lower:]
+#cat $texte_brut | tr -d [:punct:] | tr -d "«" | tr -d "—" | tr -s " " | tr " " "\n" | tr -s "\n" | tr [:upper:] [:lower:]
+texte_final=$(cat $texte_brut | grep -P -o '\p{Latin}+' | tr [:upper:] [:lower:] | tr 'ÉÈÀ' 'éèà')
+
+echo "$texte_final"
